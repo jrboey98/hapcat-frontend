6c82419aa559f34b52f2f06b17813f0fd4d1e1cc
Alex Corkwell <i.am.the.memory@gmail.com>
2018-03-06 16:24:22 -0500

    Copy bootstrap files to www/lib with gulp
    Copy the bootstrap CSS and JS files to www/lib for usage with gulp, and
    try to hook gulp into Cordova, so that Cordova will run the proper gulp
    commands if needed.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,27 @@
 var gulp = require('gulp');
 var cordova = require('cordova-lib').cordova;
 
-gulp.task('default', function(callback) {
-  cordova.build({
-    'platforms': ['android'],
-    'options': {
-      argv: ['--gradleArg=--no-daemon']
-    }
-  }, callback);
+
+/// <binding BeforeBuild='add-deps' />
+
+
+gulp.task('build-android',
+  ['add-deps'],
+  function(callback) {
+    cordova.build({
+      'platforms': ['android'],
+      'options': {
+        argv: ['--gradleArg=--no-daemon']
+      }
+    }, callback);
 });
+gulp.task('add-deps', ['add-bootstrap']);
+
+
+gulp.task('add-bootstrap', function() {
+  return gulp.src('node_modules/bootstrap/dist/**/*.+(js|css)')
+    .pipe(gulp.dest('www/lib/bootstrap'))
+});
+
+
+gulp.task('default', ['build-android']);
