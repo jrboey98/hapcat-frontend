0e0636978d793c807993042f924d94dd5251ff90
jrboey98 <jboey@kent.edu>
2018-04-02 18:21:01 -0400

    Add icons to navbar at bottom

--- www/js/index.js
+++ www/js/index.js
@@ -448,41 +448,7 @@ function getLorem() {
     return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 }
 
-/*
- * Replace all SVG images with inline SVG
- */
-//jQuery('img.svg').each(function () {
-//    var $img = jQuery(this);
-//    var imgID = $img.attr('id');
-//    var imgClass = $img.attr('class');
-//    var imgURL = $img.attr('src');
 
-//    jQuery.get(imgURL, function (data) {
-//        // Get the SVG tag, ignore the rest
-//        var $svg = jQuery(data).find('svg');
 
-//        // Add replaced image's ID to the new SVG
-//        if (typeof imgID !== 'undefined') {
-//            $svg = $svg.attr('id', imgID);
-//        }
-//        // Add replaced image's classes to the new SVG
-//        if (typeof imgClass !== 'undefined') {
-//            $svg = $svg.attr('class', imgClass + ' replaced-svg');
-//        }
-
-//        // Remove any invalid XML tags as per http://validator.w3.org
-//        $svg = $svg.removeAttr('xmlns:a');
-
-//        // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
-//        if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-//            $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-//        }
-
-//        // Replace image with new SVG
-//        $img.replaceWith($svg);
-
-//    }, 'xml');
-
-//});
 
 app.initialize();
